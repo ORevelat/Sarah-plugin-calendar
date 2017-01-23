9b6be11974a2feaf401e54893edc13c20c73c98b
Olivier <olivier.revelat@gmail.com>
2017-01-23 19:42:56 +0100

    Suppression de la demande de la semaine en cours (transférer sur le plugin time)

--- calendar.js
+++ calendar.js
@@ -47,8 +47,8 @@ var actionCalendar = function (data, next) {
 
 	if (data.cmd == "PLANNING")
 		return actionPlanning(data, next);
-	else if (data.cmd == "WEEK")
-		return actionWeek(data, next);
+
+	commandError(next);
 };
 
 var actionPlanning = function (data, next) {
@@ -121,38 +121,10 @@ var actionPlanning = function (data, next) {
 	});
 };
 
-var actionWeek = function (data, next) {
-	var pluginProps = Config.modules.calendar;
-
-	var weeknumber = weekNumber();
-	var tts = 'nous sommes ';
-	if (data.check == 'tomorrow') {
-		weeknumber += 1;
-		tts = 'nous serons ';
-	}
-
-	tts = tts + ' en semaine ' + weeknumber + ' ( semaine ' + ((weeknumber%2 == 0) ? 'A' : 'B') + ' pour théa )';
-
-	next({'tts': tts });
-};
 
 function toDay(day) {
 	var days = ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'];
 	return days[day];
-};
-
-function weekNumber() {
-	var date = new Date();
-	date.setHours(0, 0, 0, 0);
-  
-  	// Thursday in current week decides the year.
-  	date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-  
-	// January 4 is always in week 1.
-	var week1 = new Date(date.getFullYear(), 0, 4);
-
-	// Adjust to Thursday in week 1 and count number of weeks from date to week1.
-	return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
 };
 
 // GOOGLE API specific
